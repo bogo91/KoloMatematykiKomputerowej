9073c7f102270156b5133739eef57ad87f77959f
ingwarus <igor-as@o2.pl>
2015-05-02 13:22:05 +0200

    Update Cell.py

--- Cell.py
+++ Cell.py
@@ -3,26 +3,25 @@ __author__ = 'malin & ingwarus'  # dobra dobra :P
 
 class Cell:
     """
-    Cell to uogólnienie sympleksu/kostki/elementu jakiegoś bardziej chorego kompleksu
-    z tego będziemy sobie dziedziczyć do sympleksu i kostki.
-    """
+                        Cell to uogólnienie sympleksu/kostki/elementu jakiegoś bardziej chorego kompleksu
+                        z tego będziemy sobie dziedziczyć do sympleksu i kostki.
 
-    """
-        :ivar id_counter: liczba wszytskich cellow
+                        :ivar id_counter: liczba wszytskich cellow
     """
     id_counter = 0
 
     def __init__(self, dim: int, label=None):
-        """ Inicjalizuje zmienne wewnetrzne obiektu
+        """ 
+                                Inicjalizuje zmienne wewnetrzne obiektu
 
-        :type self.id: int
-        :param dim: wymiar cell'a
-        :param label: domyslnie pusty, nazwa cell'a
-        :type label: str
-        :type dim: int
-        :type self.cofaces: list
-        :type self.faces: list
-        :ivar id: unikatowy numer dla kazdego cella
+                                :type self.id: int
+                                :param dim: wymiar cell'a
+                                :param label: domyslnie pusty, nazwa cell'a
+                                :type label: str
+                                :type dim: int
+                                :type self.cofaces: list
+                                :type self.faces: list
+                                :ivar id: unikatowy numer dla kazdego cella
         """
         self.dim = dim
         self.label = label
@@ -32,25 +31,25 @@ class Cell:
         Cell.id_counter += 1
 
     def append_coface(self, cell):
-        """Dodaje cell'a 'w gore' w grafie
-        :type cell: Cell
-        :param cell: node do dodania
+        """ 
+                                    Dodaje cell'a 'w gore' w grafie
+                                    :type cell: Cell
+                                    :param cell: node do dodania
         """
         assert isinstance(cell, Cell), "Blad! Dodany obiekt nie jest cellem"
         self.cofaces.add(cell)
 
     def append_face(self, cell):
         """ Dodaje cell'a 'w dol' w grafie
-        :type cell: Cell
-        :param cell: node do dodania
+                                    :type cell: Cell
+                                    :param cell: node do dodania
         """
         assert isinstance(cell, Cell), "Blad! Dodany obiekt nie jest cellem"
         self.faces.add(cell)
 
     def connect(self, cell):
         """metoda laczy cell z obecnym
-
-        :param cell: do polaczenia cell
+                                    :param cell: do polaczenia cell
         """
         Cell.connect(self, cell)
 
