7f1f3ad25a8a989e624110241f3215eef5167b1e
Malin <MaLiN2223@users.noreply.github.com>
2015-05-03 19:41:55 +0200

    Update CubicalSignature.py

--- Structures/CubicalSignature.py
+++ Structures/CubicalSignature.py
@@ -2,5 +2,24 @@ __author__ = 'malin'
 
 
 class CubicalSignature:
+
     def __init__(self, sig):
-        self.dim = sum(len(element) for element in sig) - len(sig) 
+        self.sig = sig
+        self.dim = sum(len(element) for element in sig) - len(sig)
+
+    def get_faces(self):
+        faces = []
+        for interval in self.sig:
+            face = []
+            for i in self.sig:
+                if i is interval:
+                    face.append(i[0])
+                else:
+                    face.append(i)
+            if len(interval)==2:
+                for i in self.sig:
+                    if i is interval:
+                        face.append(i[1])
+                    else:
+                        face.append(i)
+            faces.append(face)
