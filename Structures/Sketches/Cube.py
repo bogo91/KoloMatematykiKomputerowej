45150734219b6d0737c3b587d442eba5ab62eb41
ingwarus <igor-as@o2.pl>
2015-05-03 17:43:27 +0200

    Create Cube.py

---
+++ Structures/Sketches/Cube.py
@@ -0,0 +1,9 @@
+import Cell
+
+class Cube(Cell):
+    __init__(self, sig, cubes):
+        dim=get_sig_dim(sig)
+        for each s in get_sig_faces(sig):
+            if not s in cubes[dim-1]:
+                cubes[dim-1][s]=Cube(s,cubes)
+            self.connect(cubes[dim-1][s]
