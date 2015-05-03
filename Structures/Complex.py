445ce3dd55d58e80b39e3e805f660610bf1bfd5b
MaLiN2223 <szyszka45@gmail.com>
2015-05-03 18:31:10 +0200

    Zmiany

--- Structures/Complex.py
+++ Structures/Complex.py
@@ -1,5 +1,25 @@
 __author__ = 'malin'
 
-
+from Structures import Cell
+ 
 class Complex:
-    pass
+    def __init__(self, list):
+        """
+        :type self.skeletonCollection: list
+        """
+        self.skeletonCollection = list
+
+    def append(self, skeleton: Skeleton):
+        self.skeletonCollection.append(skeleton)
+
+    def __str__(self):
+        if self.skeletonCollection:
+            tmp = "To jest complex z lista zawierajaca :\n\n%s" % "".join(map(lambda x: "poziom %s:\n\n%s" % (x.dim, x),filter(lambda x: x.cellCollection, self.skeletonCollection)))
+        else:
+            tmp = "To jest complex, ktory jest obecnie pusty"
+        return tmp
+
+
+
+
+
