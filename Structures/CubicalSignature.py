03ef1c96c72cf7d8cd313e423294dba525086b87
MaLiN2223 <szyszka45@gmail.com>
2015-05-03 19:09:32 +0200

    Zmiany

--- Structures/CubicalSignature.py
+++ Structures/CubicalSignature.py
@@ -3,12 +3,4 @@ __author__ = 'malin'
 
 class CubicalSignature:
     def __init__(self, sig):
-        self.dim = CubicalSignature.degenerated_count(sig)
-
-    @staticmethod
-    def degenerated_count(sig):
-        i = 0
-        for element in sig:
-            if len(element) == 2:
-                i += 1
-        return i
+        self.dim = sum(len(element) for element in sig) - len(sig) 
