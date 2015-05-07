82048ce6240f5b12284f113fab637dab03be1d79
ingwarus <igor-as@o2.pl>
2015-05-07 20:59:10 +0200

    Update Cube.py

--- Structures/Cube.py
+++ Structures/Cube.py
@@ -3,18 +3,9 @@ __author__ = 'malin'
 
 
 class Cube(Cell):
-    def __init__(self, sig, cubes):
-        """
-                                        Sig ma postac [0,1],[1],[0,1],[0]
-        """
-        super().__init__(len(sig), sig)
-
-
-
-"""
-dim=get_sig_dim(sig)
-for each s in get_sig_faces(sig):
-    if not s in cubes[dim-1]:
-        cubes[dim-1][s]=Cube(s,cubes)
-    self.connect(cubes[dim-1][s]
-"""
+    __init__(self, sig, cubes):
+        dim=get_sig_dim(sig)
+        for each s in get_sig_faces(sig):
+            if not s in cubes[dim-1]:
+                cubes[dim-1][s]=Cube(s,cubes)
+            self.connect(cubes[dim-1][s])
