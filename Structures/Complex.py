57c63935260620f2209114ad321923bcbd871335
ingwarus <igor-as@o2.pl>
2015-05-07 22:56:20 +0200

    Update Complex.py

--- Structures/Complex.py
+++ Structures/Complex.py
@@ -1,13 +1,18 @@
+__author__ = 'ingwarus'
+
 from Structures import Cell
  
 class Complex:
     def __init__(self, cells):
         self.cells=cells
         self.dim=len(cells)-1
-"""
+
     def __str__(self):
-        return ""
-"""
+        s="Complex wymiaru "+str(self.dim)+" zawierajacy:\n"
+        for lvl in self.cells:
+            for cell in lvl:
+                s+=str(cell)
+        return s
 
 
 
