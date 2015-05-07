d5c056195f12d2ad75365fbc4248a9a754ec39b1
ingwarus <igor-as@o2.pl>
2015-05-07 20:40:21 +0200

    Update Complex.py

--- Structures/Complex.py
+++ Structures/Complex.py
@@ -1,23 +1,15 @@
-__author__ = 'malin'
+__author__ = 'ingwarus'
 
 from Structures import Cell
  
 class Complex:
-    def __init__(self, list):
-        """
-        :type self.skeletonCollection: list
-        """
-        self.skeletonCollection = list
-
-    def append(self, skeleton: Skeleton):
-        self.skeletonCollection.append(skeleton)
-
+    def __init__(self, cells):
+        self.cells=cells
+        self.dim=len(cells)-1
+"""
     def __str__(self):
-        if self.skeletonCollection:
-            tmp = "To jest complex z lista zawierajaca :\n\n%s" % "".join(map(lambda x: "poziom %s:\n\n%s" % (x.dim, x),filter(lambda x: x.cellCollection, self.skeletonCollection)))
-        else:
-            tmp = "To jest complex, ktory jest obecnie pusty"
-        return tmp
+        return ""
+"""
 
 
 
