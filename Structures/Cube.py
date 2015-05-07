768fa5b82544f8c24091af4207e80d359dcae6f4
ingwarus <igor-as@o2.pl>
2015-05-07 22:48:26 +0200

    Update Cube.py

--- Structures/Cube.py
+++ Structures/Cube.py
@@ -1,11 +1,15 @@
+import pdb
 from Structures.Cell import Cell
-__author__ = 'ingwarus'
 
 
 class Cube(Cell):
-    __init__(self, sig, cubes):
-        dim=get_sig_dim(sig)
-        for each s in get_sig_faces(sig):
+    def __init__(self, dim,sig, cubes):
+        Cell.__init__(self,sig.get_dim())
+        self.signature=sig
+        #pdb.set_trace()
+        for s in sig.get_faces():
             if not s in cubes[dim-1]:
-                cubes[dim-1][s]=Cube(s,cubes)
+                cubes[dim-1][s]=Cube(dim-1,s,cubes)
             self.connect(cubes[dim-1][s])
+    def __str__(self):
+        return "Cube %s: id = %s, dim = %s\n" % (self.signature, self.id, self.dim)
